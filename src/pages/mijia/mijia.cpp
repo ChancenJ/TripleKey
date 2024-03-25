d37a022c46dc3c1f901493909bec279cbafeae4b
ChancenJ <jy8866@126.com>
2024-03-25 16:53:09 +0800

    V0.0.14 米家开关全新框架，统一按键结构体，加快响应速度，米家页旋钮转动变为转一次移动一格

--- src/pages/mijia/mijia.cpp
+++ src/pages/mijia/mijia.cpp
@@ -1,61 +1,109 @@
 #include "mijia.h"
 #include "board_def.h"
 #include "app/app_key.h"
+#include "app/app_mijia.h"
+
+#define MIJIALV RGB565(25, 203, 139)
+
+struct sw
+{
+	uint8_t pin;
+	String name_cn;
+	String name_en;
+	uint8_t optype; // 1为单击，2为双击，3为长按
+	uint8_t type;	// 1为场景开关，2为普通开关
+};
+
+sw sws[] = {
+	{MIJIAKEY1_PIN, "场景1", "Scene1", 1, 1},
+	{MIJIAKEY1_PIN, "场景2", "Scene2", 2, 1},
+	{MIJIAKEY1_PIN, "场景3", "Scene3", 3, 1},
+	{MIJIAKEY1_PIN, "场景4", "Scene4", 2, 1},
+};
+
+static const uint8_t maxsw = sizeof(sws) / sizeof(sw);
+
+static int8_t firstindex;
+
+static void dispSwitch()
+{
+	int16_t x1;
+	int16_t y1;
+	uint16_t w;
+	uint16_t h;
+
+	gfx1->fillScreen(BLACK);
+	gfx2->fillScreen(BLACK);
+	gfx3->fillScreen(BLACK);
+
+	for (uint8_t i = 0; i < 3; i++)
+	{
+		gfx[i]->setTextColor(MIJIALV);
+		gfx[i]->setFont(&MiSans_Demibold_12);
+		gfx[i]->getTextBounds(sws[(firstindex + i) % maxsw].name_en, 0, 0, &x1, &y1, &w, &h);
+		gfx[i]->setCursor((OLED_WIDTH - w) / 2, (OLED_HEIGHT - h) / 2 - y1);
+		gfx[i]->print(sws[(firstindex + i) % maxsw].name_en);
+	}
+}
 
 static void init(void *data)
 {
-	
+	digitalWrite(MIJIAKEY1_PIN, HIGH);
+	firstindex = 0;
+	Serial.println(maxsw);
 }
 
 static void enter(void *data)
 {
 	// insert code
-	gfx1->fillScreen(BLACK);
-	gfx2->fillScreen(BLACK);
-	gfx3->fillScreen(BLACK);
+	dispSwitch();
 
-	gfx1->setFont(&MiSans_Demibold_12);
-	gfx1->setCursor(20, 50);
-	gfx1->print("KEY5");
-	
 	//
 	manager_setBusy(false);
 }
 
 static void loop(void *data)
 {
-	delay(100);
 	KEY_TYPE key;
 	key = app_key_get();
 	switch (key)
 	{
-
-	case KEY1_SHORT:
-		Serial.println("米家单击");
-		digitalWrite(MIJIAKEY1_PIN,LOW);
-		delay(100);
-		digitalWrite(MIJIAKEY1_PIN,HIGH);
-		delay(100);
+	case KEY1_DOWN:
+		app_led_set(LED1, app_led_color(25, 203, 139));
+		app_led_update();
+		app_mijia_control(sws[firstindex].pin, sws[firstindex].optype);
 		break;
-		
-	case KEY1_DOUBLE:
-		Serial.println("米家双击");
-		digitalWrite(MIJIAKEY1_PIN,LOW);
-		delay(100);
-		digitalWrite(MIJIAKEY1_PIN,HIGH);
-		delay(100);
-		digitalWrite(MIJIAKEY1_PIN,LOW);
-		delay(100);
-		digitalWrite(MIJIAKEY1_PIN,HIGH);
-		delay(100);
+	case KEY2_DOWN:
+		app_led_set(LED2, app_led_color(25, 203, 139));
+		app_led_update();
+		app_mijia_control(sws[(firstindex + 1) % maxsw].pin, sws[(firstindex + 1) % maxsw].optype);
+		break;
+	case KEY3_DOWN:
+		app_led_set(LED3, app_led_color(25, 203, 139));
+		app_led_update();
+		app_mijia_control(sws[(firstindex + 2) % maxsw].pin, sws[(firstindex + 2) % maxsw].optype);
+		break;
+	case KEY1_UP:
+	case KEY2_UP:
+	case KEY3_UP:
+		app_led_off();
 		break;
 
-	case KEY1_LONG:
-		Serial.println("米家长按");
-		digitalWrite(MIJIAKEY1_PIN,LOW);
-		delay(2000);
-		digitalWrite(MIJIAKEY1_PIN,HIGH);
-		delay(100);
+	case ENC_NEXT:
+		firstindex++;
+		if (firstindex >= maxsw)
+		{
+			firstindex = 0;
+		}
+		dispSwitch();
+		break;
+	case ENC_PREV:
+		firstindex--;
+		if (firstindex < 0)
+		{
+			firstindex = maxsw - 1;
+		}
+		dispSwitch();
 		break;
 
 	case KEY4_LONG:				  // 长按
