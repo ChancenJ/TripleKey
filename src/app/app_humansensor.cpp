c4498f59e4969fc046e122ef3905724f989ba790
ChancenJ <jy8866@126.com>
2024-04-10 01:42:33 +0800

    V0.2.19 优化人在传感器

--- src/app/app_humansensor.cpp
+++ src/app/app_humansensor.cpp
@@ -1,10 +1,12 @@
 #include "app/app_humansensor.h"
 
+unsigned long lastSensorUpdateTime = millis();
+
 uint8_t getHumanSensorState()
 {
 #ifdef SUPPORT_CH423S
     uint8_t value = ch423.digitalRead(ch423.eGPIO7);
-    //Serial.printf("HumanState: %d\n",value);
+    // Serial.printf("HumanState: %d\n",value);
     return value;
 #else
     return 0;
@@ -13,11 +15,21 @@ uint8_t getHumanSensorState()
 
 void Mijia_UpdateHumanState()
 {
-    uint8_t humanstate = getHumanSensorState();
-    uint8_t swstate = app_mijia_get(K1);
-    if(humanstate!=swstate){
+    if (millis() - lastSensorUpdateTime <= 200)
+    {
+        return;
+    }
+    lastSensorUpdateTime = millis();
+#ifdef SUPPORT_CH423S
+    uint8_t value = ch423.digitalRead(ch423.eGPIOTotal);
+    uint8_t humanstate = (value & (1 << 7)) >> 7; // GPIO7
+    uint8_t swstate = value & 1;                  // GPIO0
+    // uint8_t humanstate = getHumanSensorState();
+    // uint8_t swstate = app_mijia_get(K1);
+    if (humanstate != swstate)
+    {
         app_mijia_short(K1);
         Serial.println("有人无人状态切换");
     }
-    
+#endif
 }
