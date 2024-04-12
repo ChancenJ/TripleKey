b071158a27b5a3b98d1ce1f840b6eccbac379605
ChancenJ <jy8866@126.com>
2024-04-13 03:19:24 +0800

    V0.3.18 设置页增加二维码，扫码配置

--- src/pages/settings/settings.cpp
+++ src/pages/settings/settings.cpp
@@ -3,18 +3,49 @@
 #include "app/app_key.h"
 #include "app/app_settings.h"
 
-void dispInfo(){
+#include "QRCode_Library.h"
+
+void dispInfo()
+{
 	gfx[0]->fillScreen(BLACK);
 	gfx[0]->setFont(u8g2_font_6x10_mr);
 	gfx[0]->setTextColor(QINGSHUILAN);
-	gfx[0]->setCursor(0,10);
+	gfx[0]->setCursor(0, 10);
 	gfx[0]->println("Enter the URL:");
 	gfx[0]->println(WiFi.localIP());
 }
+void drawQRCode(QRCode qrcode, uint8_t magnification /*放大倍数*/, uint8_t xpos /*左上角x坐标*/, uint8_t ypos /*左上角y坐标*/,uint8_t screen/*屏幕*/)
+{
+	for (uint8_t y = 0; y < qrcode.size; y++)
+	{
+		for (uint8_t x = 0; x < qrcode.size; x++)
+		{
+			if (qrcode_getModule(&qrcode, x, y))
+			{
+				gfx[screen]->fillRect(x * magnification + xpos, y * magnification + ypos, magnification, magnification, WHITE);
+			}
+			else
+			{
+				gfx[screen]->fillRect(x * magnification + xpos, y * magnification + ypos, magnification, magnification, BLACK);
+			}
+		}
+	}
+}
+void dispQRCode()
+{
+	QRCode qrcode;
+	char ip[20];
+	WiFi.localIP().toString().toCharArray(ip, 20);
+	uint8_t qrcodeBytes[qrcode_getBufferSize(3)];
+	qrcode_initText(&qrcode, qrcodeBytes, 3, ECC_LOW, ip);
+	Serial.println(qrcode.size);
+	drawQRCode(qrcode,3,(OLED_WIDTH-qrcode.size*3)/2,5,1);
+}
+
 
 static void gotoConfig()
 {
-	String url= WiFi.localIP().toString();
+	String url = WiFi.localIP().toString();
 	bleKeyboard.releaseAll();
 	bleKeyboard.write(KEY_MEDIA_WWW_HOME);
 	delay(800);
@@ -22,7 +53,7 @@ static void gotoConfig()
 	delay(200);
 	bleKeyboard.write(KEY_DELETE);
 	delay(200);
-	bleKeyboard.write(KEY_RETURN); //回车
+	bleKeyboard.write(KEY_RETURN); // 回车
 }
 
 static void init(void *data)
@@ -41,6 +72,7 @@ static void enter(void *data)
 		delay(500);
 	}
 	dispInfo();
+	dispQRCode();
 	Serial.println("");
 	Serial.print("Connected to WiFi. IP address: ");
 	Serial.println(WiFi.localIP());
@@ -74,6 +106,8 @@ static void loop(void *data)
 
 	case KEY1_DOWN:
 		gotoConfig();
+		break;
+	case KEY3_DOWN:
 		break;
 
 	case KEY4_LONG:				  // 长按
